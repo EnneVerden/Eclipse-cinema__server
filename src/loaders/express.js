2b1dbe106b8571919acc29ce9d6bf2376c45014a
Pavel Adamovich <42992218+EnneVerden@users.noreply.github.com>
2020-04-24 13:46:42 +0300

    Added cors.

--- src/loaders/express.js
+++ src/loaders/express.js
@@ -1,8 +1,10 @@
+const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const express = require("express");
 
 module.exports = (expressApp) => {
   expressApp
+    .use(cors())
     .use(cookieParser())
     .use(express.urlencoded({ extended: false }))
     .use(express.json());
